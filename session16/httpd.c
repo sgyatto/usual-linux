45462a3396edbd1801f76b86ebc7e334191a7461
sgyatto <42696711+sgyatto@users.noreply.github.com>
2019-06-18 23:41:16 +0900

    add install_signal_handlers()

--- session16/httpd.c
+++ session16/httpd.c
@@ -1,13 +1,40 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
 
 /* Function Prototypes **************/
 
+typedef void (*sighandler_t)(int);
+static void install_signal_handlers(void);
+static void trap_signal(int sig, sighandler_t handler);
+static void signal_exit(int sig);
 static void *xmalloc(size_t sz);
 static void log_exit(char *fmt, ...);
 
 /* Functions ************************/
+
+static void install_signal_handlers(void)
+{
+	trap_signal(SIGPIPE, signal_exit);
+}
+
+static void trap_signal(int sig, sighandler_t handler)
+{
+	struct sigaction act;
+
+	act.sa_handler = handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_RESTART;
+	if (sigaction(sig, &act, NULL) < 0)
+		log_exit("sigaction() failed: %s", strerror(errno));
+}
+
+static void signal_exit(int sig)
+{
+	log_exit("exit by signal %d", sig);
+}
 
 static void *xmalloc(size_t sz)
 {
