b511d1f4807221434afd24c24bf80b405f7a0933
sgyatto <42696711+sgyatto@users.noreply.github.com>
2019-06-19 00:03:18 +0900

    add main()

--- session16/httpd.c
+++ session16/httpd.c
@@ -15,6 +15,17 @@ static void log_exit(char *fmt, ...);
 
 /* Functions ************************/
 
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
+		exit(1);
+	}
+	install_signal_handlers();
+	service(stdin, stdout, argv[1]);
+	exit(0);
+}
+
 static void install_signal_handlers(void)
 {
 	trap_signal(SIGPIPE, signal_exit);
